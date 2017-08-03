4e121db72807c6622a36de8b4117ee38148ded27
Anil Shafeeque <anil.shafeeque@gmail.com>
2017-08-03 21:02:42 +0300

    Update app.py
    host changed from redis to VM IP

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import socket
 from flask import render_template
  
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+redis = Redis(host='192.168.100.20', port=6379)
  
 hostname = socket.gethostname()
 @app.route('/')
