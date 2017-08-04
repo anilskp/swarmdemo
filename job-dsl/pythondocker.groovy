039fa0af2b8b3150228a242db0375aa36b527e91
Anil Shafeeque <anil.shafeeque@gmail.com>
2017-08-04 08:01:35 +0300

    Update pythondocker.groovy
    Added deploy step

--- job-dsl/pythondocker.groovy
+++ job-dsl/pythondocker.groovy
@@ -16,5 +16,8 @@ job('PythonFlask Docker example') {
             createFingerprints(false)
             skipDecorate()
         }
+    steps {
+        shell(readFileFromWorkspace('deploycontainer.txt'))
+         }      
     }
 }
