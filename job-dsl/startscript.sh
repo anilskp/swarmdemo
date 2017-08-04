c3e717b380abd17c675626008192cc2bd6b9642b
Anil Shafeeque <anil.shafeeque@gmail.com>
2017-08-04 14:06:37 +0300

    Update startscript.sh

--- job-dsl/startscript.sh
+++ job-dsl/startscript.sh
@@ -1,2 +1,2 @@
 docker service rm swarmdemo
-docker service create  -p 5500:5000  --name swarmdemo  --limit-cpu=2 --replicas=3  anilskp/swarmdemo
+docker service create  -p 5500:5000  --name swarmdemo  --limit-cpu=1 --replicas=3  anilskp/swarmdemo
