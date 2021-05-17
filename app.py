13307728b7f2665863cc8055cb46ae76d642d8b3
Anil Shafeeque <anil.shafeeque@gmail.com>
2021-05-17 14:35:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ import socket
 from flask import render_template
  
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+#redis = Redis(host='redis', port=6379)
  
 hostname = socket.gethostname()
 @app.route('/')
 def hello():
  
-    count = redis.incr('hits')
-#    count = 1
+#    count = redis.incr('hits')
+    count = 1
     displaystring =  " " + str(count) +" Times and now from the host  " + hostname
  
 #    return 'Hello World! I have been seen {}  .\n'.format (displaystring )
